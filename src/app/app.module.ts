424f38384d6ad18d4b0446e1667a12dfbde49e23
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-11-05 12:54:39 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoViewComponent } from './todo-view/todo-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemComponent,
+    TodoItemComponent,
+    TodoViewComponent
   ],
   imports: [
     BrowserModule
